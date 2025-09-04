7143ee1546bca7481cab261a0057ddca25f65864
João Vitor Parizotto Rocha <jvprocha.05@gmail.com>
2025-09-04 11:44:30 -0300

    estilizando fonte

--- src/components/CalcularFrete.jsx
+++ src/components/CalcularFrete.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const CalcularFrete = () => {
   return (
-    <div>CalcularFrete</div>
+    <div className='flex justify-center items-center min-h-screen bg-gray-400 p-4'>
+        <div className='bg-white p-8 rounded-2x1 max-w-md text-center'>
+          <h1 className='text-3x1 font-bold text-blue-600 mb-6'>Calculadora de frete</h1>
+        </div>
+    </div>
   )
 }
 
